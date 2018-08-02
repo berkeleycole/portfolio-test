ea202f70b4d0f922b4859794b4b71addb2c54f48
Alyssa Hope <alyssamhope@gmail.com>
2018-08-02 15:22:04 -0700

    intentional typo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './resets.css';
-
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-// missing import for <App />
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
