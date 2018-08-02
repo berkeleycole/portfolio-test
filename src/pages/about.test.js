1b8b94205ff78d9f997733bb12b72573745e64e0
Alyssa Hope <alyssamhope@gmail.com>
2018-08-02 11:08:39 -0700

    creating fresh failing branch

--- src/pages/about.test.js
+++ src/pages/about.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import About from './about';
+import About from './abouts';
 
 describe('rendering the about page', () => {
 	it('renders without crashing', () => {
